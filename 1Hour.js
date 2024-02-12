906f6ebdf5581d3318c94a281dd990a95eaea85e
ajujawade <ajayjawade06@gmail.com>
2024-02-12 14:21:39 +0530

    Revised

--- 1Hour.js
+++ 1Hour.js
@@ -1,30 +1,76 @@
+// ////////////////////////////////////
+// // Linking a JavaScript File
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// ////////////////////////////////////
+// // Values and Variables
+// console.log("Ram");
+// console.log(23);
+
+// let firstName = "Chinmay";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// // Variable name conventions
+// let arjun_meenakshi = "AM";
+// let $function = 27;
+
+// let person = "Ajay";
+// let PI = 3.1415;
+
+// let myFirstJob = "Engineer";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "Engineer";
+// let job2 = "Teacher";
+
+// console.log(myFirstJob);
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//Revised  1 Hour
 ////////////////////////////////////
 // Linking a JavaScript File
+// In JavaScript, you can store data in variables and perform operations on them.
+// Here, we're storing the string "amazing" in a variable called 'js'.
 let js = "amazing";
+
+// You can perform mathematical operations using arithmetic operators.
+// The result of this expression will be calculated and printed to the console.
 console.log(40 + 8 + 23 - 10);
 
 ////////////////////////////////////
 // Values and Variables
-console.log("Ram");
+// You can print text and numbers directly to the console.
+console.log("Hello, world!");
 console.log(23);
 
+// 'let' is a keyword used to declare variables. Here, we declare a variable called 'firstName' and assign it the value "Chinmay".
 let firstName = "Chinmay";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// You can print the value of variables to the console using 'console.log'.
+console.log(firstName); // This will print "Chinmay".
+console.log(firstName); // It will print "Chinmay" again.
+console.log(firstName); // And again.
 
-// Variable name conventions
-let arjun_meenakshi = "AM";
-let $function = 27;
-
-let person = "Ajay";
+// Variable names can contain letters, digits, underscores, and dollar signs. They must start with a letter, dollar sign ($), or underscore (_).
+// Here are some examples of valid variable names:
+let personName = "Ajay";
 let PI = 3.1415;
 
+// Variable names are case-sensitive. 'myFirstJob' and 'myfirstjob' are different variables.
+// Variable names should be descriptive to make the code easier to understand.
 let myFirstJob = "Engineer";
 let myCurrentJob = "Teacher";
 
+// You can also use shorter variable names if they still make sense in your context.
 let job1 = "Engineer";
 let job2 = "Teacher";
 
-console.log(myFirstJob);
+// Printing the value of 'myFirstJob' to the console.
+console.log(myFirstJob); // This will print "Engineer".
